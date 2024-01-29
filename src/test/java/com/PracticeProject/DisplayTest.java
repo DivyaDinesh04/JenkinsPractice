842a4dee16a6739c468c87f741689c74180f030d
DivyaDinesh04 <divyaragavan4@gmail.com>
2024-01-29 13:54:35 +0530

    new sample

--- src/test/java/com/PracticeProject/DisplayTest.java
+++ src/test/java/com/PracticeProject/DisplayTest.java
@@ -5,12 +5,14 @@ import org.testng.annotations.Test;
 public class DisplayTest {
 
 	
-	@Test
+	@Test(groups = "smoke")
 	public void display1Test()
 	{
 		System.out.println(" display test1 ");
 	}
 	
+	
+	@Test(groups = "regression")
 	public void display2Test()
 	{
 		System.out.println(" display test2 ");
--- src/test/java/com/PracticeProject/SampleTest.java
+++ src/test/java/com/PracticeProject/SampleTest.java
@@ -4,17 +4,19 @@ import org.testng.annotations.Test;
 
 public class SampleTest {
 	
-	@Test
+	@Test(groups = "smoke")
 	public void sample1Test()
 	{
 		System.out.println(" sample test1 ");
 	}
 	
+	@Test(groups = "smoke")
 	public void sample2Test()
 	{
 		System.out.println(" sample test2 ");
 	}
 	
+	@Test(groups = {"smoke","regression"})
 	public void sample3Test()
 	{
 		System.out.println(" new sample test3 ");
