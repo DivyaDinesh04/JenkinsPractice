b18fe9b63ff793e46aa9ae9bfc01b01c956aeec1
DivyaDinesh04 <divyaragavan4@gmail.com>
2024-01-29 13:22:03 +0530

    jenkins

---
+++ src/test/java/com/PracticeProject/DisplayTest.java
@@ -0,0 +1,18 @@
+package com.PracticeProject;
+
+import org.testng.annotations.Test;
+
+public class DisplayTest {
+
+	
+	@Test
+	public void display1Test()
+	{
+		System.out.println(" display test1 ");
+	}
+	
+	public void display2Test()
+	{
+		System.out.println(" display test2 ");
+	}
+}
---
+++ src/test/java/com/PracticeProject/SampleTest.java
@@ -0,0 +1,18 @@
+package com.PracticeProject;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void sample1Test()
+	{
+		System.out.println(" sample test1 ");
+	}
+	
+	public void sample2Test()
+	{
+		System.out.println(" sample test2 ");
+	}
+
+}
